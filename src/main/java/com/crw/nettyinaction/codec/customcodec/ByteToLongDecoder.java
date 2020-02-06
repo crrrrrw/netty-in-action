690084ed8eac18e050fdcbc41bc6667532da4e6e
crrrrrw <120755274@qq.com>
2020-02-06 21:46:58 +0800

    当数据不匹配时的源码分析

--- src/main/java/com/crw/nettyinaction/codec/customcodec/ByteToLongDecoder.java
+++ src/main/java/com/crw/nettyinaction/codec/customcodec/ByteToLongDecoder.java
@@ -9,6 +9,11 @@ import java.util.List;
 public class ByteToLongDecoder extends ByteToMessageDecoder {
 
     /**
+     * decode 会根据接收到的数据被调用多次，直到确定了没有新的元素被添加到list中，
+     * 或者 byteBuf 没有更多的可读字节为止。
+     * 如果 list 里不为空，就会把 list 的内容传递给下一个 ChannelInboundHandler，
+     * 该处理器的方法也会被调用多次
+     *
      * @param ctx 上下文对象
      * @param in  入站的byteBuf
      * @param out List集合，将解码后的数据传给下一个handler
--- src/main/java/com/crw/nettyinaction/codec/customcodec/MyClientHandler.java
+++ src/main/java/com/crw/nettyinaction/codec/customcodec/MyClientHandler.java
@@ -1,7 +1,9 @@
 package com.crw.nettyinaction.codec.customcodec;
 
+import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.util.CharsetUtil;
 
 public class MyClientHandler extends SimpleChannelInboundHandler<Long> {
     @Override
@@ -11,8 +13,53 @@ public class MyClientHandler extends SimpleChannelInboundHandler<Long> {
 
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
-        Long msg = 123123123L;
-        System.out.println("client send msg : " + msg);
-        ctx.writeAndFlush(msg);
+        // Long msg = 123123123L;
+        // System.out.println("client send msg : " + msg);
+        // ctx.writeAndFlush(msg);
+
+        /**
+         * 1. 传送一个16字节的字符串，测试 ByteToLongDecoder decode方法调用次数 ： 2次
+         * 2. 但是 LongToByteEncoder encode方法没有被调用，why？
+         * 原因：LongToByteEncoder extends MessageToByteEncoder<Long> ，
+         * MessageToByteEncoder的write方法中：
+            public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception {
+                ByteBuf buf = null;
+                 try {
+                    if (this.acceptOutboundMessage(msg)) { // 判断当前数据是不是应该处理的类型,如果是则处理，不是则跳过 encode方法
+                        I cast = msg;
+                        buf = this.allocateBuffer(ctx, msg, this.preferDirect);
+
+                         try {
+                            this.encode(ctx, cast, buf);
+                         } finally {
+                            ReferenceCountUtil.release(msg);
+                         }
+
+                         if (buf.isReadable()) {
+                            ctx.write(buf, promise);
+                         } else {
+                            buf.release();
+                            ctx.write(Unpooled.EMPTY_BUFFER, promise);
+                         }
+
+                         buf = null;
+                    } else {
+                        ctx.write(msg, promise);
+                    }
+             } catch (EncoderException var17) {
+                                throw var17;
+             } catch (Throwable var18) {
+                                throw new EncoderException(var18);
+             } finally {
+                if (buf != null) {
+                    buf.release();
+                }
+
+            }
+
+         }
+
+         */
+        ctx.writeAndFlush(Unpooled.copiedBuffer("shdkjhwuhsjkdh", CharsetUtil.UTF_8));
     }
 }
